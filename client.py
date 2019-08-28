edfb3f5471bb228a4f199eeac6eff8624e731ce3
eagmarquesdev <eagmarquesdev@gmail.com>
2019-08-28 15:21:51 -0300

    Update client.py
    Client with reconnection problem solved.

--- client.py
+++ client.py
@@ -1,7 +1,8 @@
 import paho.mqtt.client as mqtt_client
 from time import sleep
 
-class mqtt(object):
+
+class Mqtt(object):
 
     def __init__(self, host, port, username="", password="", topic=""):
 
@@ -13,61 +14,23 @@ class mqtt(object):
         self.password = password
         self.client_id = "teste-mqtt-client-as-class"
         self.topic = topic
-        self.client = mqtt_client.Client(self.client_id, True)
-
-    def on_connect(self, client, userdata, flags, rc):
-
-        print("Connection returned result: " + str(rc))
-
-        if rc == 0:
-            self.is_connected = True
-        if self.is_subscribed:
-            self.subscribe(self.topic)
-
-    def on_disconnect(self, client, userdata, rc):
-
-        print("Disconnecting reason  " + str(rc))
-
-        self.is_connected = False
-
-    def on_subscribe(self, client, userdata, mid, granted_pos):
-
-        print("Subscribe event start")
-
-        self.is_subscribed = True
-
-    def on_unsubscribe(self, client, userdata, mid, granted_pos):
-
-        print("Unsubscribe envent start")
-
-    def on_message(self, client, userdata, msg):
-
-        received_msg = str(msg.payload)
-
-        print("[MSG RECEBIDA] Topico: " + msg.topic + " / Mensagem: " + received_msg)
-
-    def set_callbacks(self):
-
-        self.client.on_connect = self.on_connect
-        self.client.on_disconnect = self.on_disconnect
-        self.client.on_subscribe = self.on_subscribe
-        self.client.on_unsubscribe = self.on_unsubscribe
-        self.client.on_message = self.on_message
+        self.subscribed_topics_array = []
 
     def connect(self, keepalive=10):
 
-        # self.client.on_connect = self.on_connect
-        # self.client.on_disconnect = self.on_disconnect
-        # self.client.on_subscribe = self.on_subscribe
-        # self.client.on_unsubscribe = self.on_unsubscribe
-        # self.client.on_message = self.on_message
         try:
+            self.client = mqtt_client.Client(self.client_id, True)
             self.set_callbacks()
-            self.client.connect(self.host, self.port, keepalive)
-            # self.client.loop_forever()
-            self.client.loop_start()
+            sleep(0.2)
+
+            if not self.is_connected:
+                self.client.connect(self.host, self.port, keepalive)
+                self.client.loop_start()
+                sleep(0.2)
+
         except Exception as e:
-            self.disconnect()
+            if self.is_connected:
+                self.disconnect()
             print("-> Connect Exception <-")
 
     def disconnect(self):
@@ -75,17 +38,24 @@ class mqtt(object):
         try:
             if self.is_connected:
                 self.client.disconnect()
-                sleep(0.5)
+                sleep(0.2)      # era sleep(0.5)
                 self.is_connected = False
         except Exception as e:
             self.disconnect()
             print("-> Disconnect Exception <-")
 
-    def reconnect(self):
+    #Não está sendo utilizado, o propio modulo paho.mqtt.client (biblioteca) está tratando a reconexão
+    def reconnect(self, keepalive=10):
 
         try:
             if not self.is_connected:
-                self.client.reconnect()
+                self.connect()
+                sleep(0.2)
+
+            if self.subscribed_topics_array:
+                if self.is_connected:
+                    for topic in self.subscribed_topics_array:
+                        self.subscribe(topic)
         except Exception as e:
             self.disconnect()
             print("-> Reconnect Exception <-")
@@ -102,30 +72,82 @@ class mqtt(object):
             self.disconnect()
             print("-> Publish Exception <-")
 
-    def subscribe(self, topic):
+    def subscribe(self, topic="#"):
 
         self.topic = topic
         try:
             if self.is_connected:
                 self.client.subscribe(topic, qos=1)
-                # self.disconnect()
+
             else:
-                self.reconnect()
+                self.connect()
+                sleep(0.2)
                 self.subscribe(topic)
+                sleep(0.2)
+                if self.is_subscribed:
+                    self.subscribed_topics_array.append(topic)
         except Exception as e:
             self.disconnect()
             print("-> Subscribe Exception <-")
 
-var_mqtt = mqtt("10.0.0.66", 1883)
-var_mqtt.connect()
-# var_mqtt.publish("Teste de publish")
-var_mqtt.subscribe("#")
-var_mqtt.publish("teste/inventory", "Teste de pubslish!!!")
-sleep(60)
-# var_mqtt = mqtt.connect(mqtt, mqtt.host, mqtt.port, 10)
+    def set_callbacks(self):
 
-# Informações uteis:
-#                   link problema resubscribe:      https://stackoverflow.com/questions/57395474/mqtt-doesnt-send-data-to-subscriber-after-reconnection
-#                   outro link:                     https://stackoverflow.com/questions/36429609/mqtt-paho-python-reliable-reconnect
+        self.client.on_connect = self.on_connect
+        self.client.on_disconnect = self.on_disconnect
+        self.client.on_subscribe = self.on_subscribe
+        self.client.on_unsubscribe = self.on_unsubscribe
+        self.client.on_message = self.on_message
+        self.client.on_publish = self.on_publish
 
+    def on_connect(self, client, userdata, flags, rc):
 
+        print("Connection returned result: " + str(rc))
+
+        if rc == 0:
+            self.is_connected = True
+
+    def on_disconnect(self, client, userdata, rc):
+
+        print("Disconnecting reason  " + str(rc))
+
+        self.client.loop_stop()
+        self.is_connected = False
+        self.is_subscribed = False
+
+    def on_subscribe(self, client, userdata, mid, granted_pos):
+
+        print("Subscribe event start")
+
+        self.is_subscribed = True
+
+    def on_unsubscribe(self, client, userdata, mid, granted_pos):
+
+        print("Unsubscribe envent start")
+
+        self.is_subscribed = False
+
+    def on_publish(self, client, userdata, mid):
+
+        print("Data published, mid: " + mid)
+
+    def on_message(self, client, userdata, msg):
+
+        received_msg = str(msg.payload)
+
+        print("[MSG RECEBIDA]/Topico: " + msg.topic)
+        print("Mensagem: " + received_msg)
+        print("Mensagem: " + received_msg)
+
+var_mqtt = Mqtt("10.0.0.66", 1883, "Teste_Eduardo")
+
+while True:
+    if not var_mqtt.is_connected:
+        var_mqtt.connect()
+        sleep(0.2)
+        if var_mqtt.is_connected:
+            var_mqtt.subscribe("#")
+            sleep(0.2)
+
+'''Informações uteis:
+link problema resubscribe: https://stackoverflow.com/questions/57395474/mqtt-doesnt-send-data-to-subscriber-after-reconnection
+outro link: https://stackoverflow.com/questions/36429609/mqtt-paho-python-reliable-reconnect'''
